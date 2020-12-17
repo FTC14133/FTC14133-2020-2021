ac04865c6cb2548c39d62e46dfec82f1a5876929
g-schi <gschiedt25@gmail.com>
2020-12-16 21:10:20 -0500

    more teleop the sequel sequel beam break

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_14133_2021.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_14133_2021.java
@@ -18,9 +18,12 @@ public class FTC_14133_2021 extends OpMode {
     private DcMotor Shooter = null;
     HardwarePushbot robot = new HardwarePushbot();
     private DcMotor LongArm = null;
-    private DcMotor Conveyor_Belt_Inner = null;
-    private DcMotor Conveyor_Belt_Outer = null;
+    private DcMotor intake = null;
+    private DcMotor conveyor = null;
     DigitalChannel LimitSwitchLongArm ;
+    DigitalChannel beamBreak;
+
+
     public void init() {
         leftfront = hardwareMap.get(DcMotor.class, "left_drive");       //sets the names of the motors on the hardware map
         rightfront = hardwareMap.get(DcMotor.class, "right_drive");
@@ -28,9 +31,10 @@ public class FTC_14133_2021 extends OpMode {
         rightback = hardwareMap.get(DcMotor.class, "right_drive");
         LongArm = hardwareMap.get(DcMotor.class, "Long_Arm");
         Shooter = hardwareMap.get(DcMotor.class, "Shooter");
-        Conveyor_Belt_Inner = hardwareMap.get(DcMotor.class, "Conveyor_Belt");
-        Conveyor_Belt_Outer = hardwareMap.get(DcMotor.class, "Conveyor_Belt");
+        intake = hardwareMap.get(DcMotor.class, "Conveyor_Belt");
+        conveyor = hardwareMap.get(DcMotor.class, "Conveyor_Belt");
         LimitSwitchLongArm = hardwareMap.get(DigitalChannel.class, "LimitSwitchLongArm");
+        beamBreak = hardwareMap.get(DigitalChannel.class, "sensor_digital");
 
         Shooter.setDirection(DcMotor.Direction.FORWARD);            //sets the directions of the motors
         leftfront.setDirection(DcMotor.Direction.FORWARD);
@@ -38,6 +42,7 @@ public class FTC_14133_2021 extends OpMode {
         leftback.setDirection(DcMotor.Direction.FORWARD);
         rightback.setDirection(DcMotor.Direction.REVERSE);
         LimitSwitchLongArm.setMode(DigitalChannel.Mode.INPUT);
+        beamBreak.setMode(DigitalChannel.Mode.INPUT); // set the digital channel to input.
 
     }
 
@@ -100,11 +105,9 @@ public class FTC_14133_2021 extends OpMode {
 
 
         if (gamepad2.right_bumper) {
-            LongArm.setDirection(DcMotor.Direction.FORWARD);       //sets the long arm forward
             LongArm.setPower(1);
         } else if (gamepad2.left_bumper) {
-            LongArm.setDirection(DcMotor.Direction.REVERSE);        //sets the long arm backwards
-            LongArm.setPower(1);
+            LongArm.setPower(-1);
 
         }
 
@@ -116,21 +119,29 @@ public class FTC_14133_2021 extends OpMode {
 
 
 
-        if (gamepad1.left_bumper) {
-            Conveyor_Belt_Inner.setDirection(DcMotor.Direction.FORWARD);
-            Conveyor_Belt_Outer.setDirection(DcMotor.Direction.FORWARD);        // This makes the intake run forward
-            Conveyor_Belt_Inner.setPower(1);
-            Conveyor_Belt_Outer.setPower(1);
+        if (gamepad2.right_trigger>0) {
+            intake.setPower(1);
         }
-        if (gamepad1.right_bumper) {
-            Conveyor_Belt_Inner.setDirection(DcMotor.Direction.REVERSE);        // This makes the intake run backward
-            Conveyor_Belt_Outer.setDirection(DcMotor.Direction.REVERSE);
-            Conveyor_Belt_Inner.setPower(1);
-            Conveyor_Belt_Outer.setPower(1);
+
+        if (gamepad2.left_trigger>0) {
+            intake.setPower(-1);
+            conveyor.setPower(-1);
         }
+
+        while (beamBreak.getState() == true){
+            if  (gamepad2.left_trigger >0){
+                conveyor.setPower(-1);
+            }
+           else {
+                conveyor.setPower(1);
+            }
+        }
+
+
+
         else {
-            Conveyor_Belt_Outer.setPower(0);        // This tells the program to set the Intake, Long Arm, and Shooter
-            Conveyor_Belt_Inner.setPower(0);        //to turn them off when not being used
+            conveyor.setPower(0);        // This tells the program to set the Intake, Long Arm, and Shooter
+            intake.setPower(0);        //to turn them off when not being used
             Stopper.setPosition(0);
             LongArm.setPower(0);
             Shooter.setPower(0);
