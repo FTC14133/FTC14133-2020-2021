f4a1dbd754a5883874e56fb46266596f3d03893b
Vihaan Chhabria <chhabria.vihaan@gmail.com>
2021-01-06 19:03:26 -0500

    Commit 14 Vihaan Chhabria - Added new way to use  Claw button

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_14133_2021.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_14133_2021.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+ package org.firstinspires.ftc.teamcode;
 // https://first-tech-challenge.github.io/SkyStone/  This is the link to ALL metered of FTC
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -20,25 +20,25 @@ public class FTC_14133_2021 extends OpMode {
     private DcMotor LongArm = null;
     private DcMotor intake = null;
     private DcMotor conveyor = null;
-    DigitalChannel LimitSwitchLongArm ;
+    DigitalChannel LimitSwitchLongArm;
     DigitalChannel beamBreak;
     Servo Claw = null;
     Servo Stopper = null;
 
 
     public void init() {
-        leftfront = hardwareMap.get(DcMotor.class, "left_drive");       //sets the names of the motors on the hardware map
-        rightfront = hardwareMap.get(DcMotor.class, "right_drive");
-        leftback  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightback = hardwareMap.get(DcMotor.class, "right_drive");
-        LongArm = hardwareMap.get(DcMotor.class, "Long_Arm");
+        leftfront = hardwareMap.get(DcMotor.class, "leftfront");       //sets the names of the motors on the hardware map
+        rightfront = hardwareMap.get(DcMotor.class, "rightfront");
+        leftback = hardwareMap.get(DcMotor.class, "leftback");
+        rightback = hardwareMap.get(DcMotor.class, "rightback");
+        LongArm = hardwareMap.get(DcMotor.class, "LongArm");
         Shooter = hardwareMap.get(DcMotor.class, "Shooter");
-        intake = hardwareMap.get(DcMotor.class, "Conveyor_Belt");
-        conveyor = hardwareMap.get(DcMotor.class, "Conveyor_Belt");
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        conveyor = hardwareMap.get(DcMotor.class, "conveyor");
         LimitSwitchLongArm = hardwareMap.get(DigitalChannel.class, "LimitSwitchLongArm");
-        beamBreak = hardwareMap.get(DigitalChannel.class, "sensor_digital");
-        Claw = hardwareMap.get(Servo.class, "claw");
-        Stopper = hardwareMap.get(Servo.class, "stopper");
+        beamBreak = hardwareMap.get(DigitalChannel.class, "beamBreak");
+        Claw = hardwareMap.get(Servo.class, "Claw");
+        Stopper = hardwareMap.get(Servo.class, "Stopper");
 
 
         Shooter.setDirection(DcMotor.Direction.FORWARD);            //sets the directions of the motors
@@ -71,6 +71,7 @@ public class FTC_14133_2021 extends OpMode {
         double leftbackpower;       //Power level for leftback
         double rightfrontpower;     //Power level for rightfront
         int ClawButton = 1;
+        int a = 1;
 
         leftPowerY = -gamepad1.left_stick_y;      //find the value of y axis on the left joystick
         leftPowerX = gamepad1.left_stick_x;      //find the value of x axis on the left joystick
@@ -82,8 +83,7 @@ public class FTC_14133_2021 extends OpMode {
         rightbackpower = leftPowerY - leftPowerX + rightPowerX;
 
         NormScaling = Math.max(Math.max(Math.abs(leftfrontpower), Math.abs(rightfrontpower)), Math.max(Math.abs(leftbackpower), Math.abs(rightbackpower)));
-        if (NormScaling == 0) {
-        }
+
         if (NormScaling > 1) {
             leftfrontpower /= NormScaling;
             rightfrontpower /= NormScaling;
@@ -96,65 +96,69 @@ public class FTC_14133_2021 extends OpMode {
         rightback.setPower(rightbackpower);
 
 
+        if (gamepad2.y) {
+            //   ClawButton = ClawButton * -1;
+            a = a + 1;
 
-        if(gamepad2.y) {
-            ClawButton = ClawButton * -1;
-        }
-        if(ClawButton == 1){
-            Claw.setPosition(0);
-        }
-        if(ClawButton == -1){
-            Claw.setPosition(0.5);
-        }
-
-
-
-        if (gamepad2.right_bumper) {
-            LongArm.setPower(1);
-        } else if (gamepad2.left_bumper) {
-            LongArm.setPower(-1);
-
-        }
+            if (a < 200) {
+                Claw.setPosition(90);
+            } else if (a < 2000) {
+                Claw.setPosition(0);
+            } else {
+                a = 0;
+            }
 
 
-        if (gamepad2.b) {
-            Shooter.setPower(1);            // This Controls the shooter
-            Stopper.setPosition(0.5);        // This sets the Stopper to allow rings to come in the Shooter
-        }
+            //  }
+            //if(ClawButton == 1){
+            //  Claw.setPosition(0);
+            //  }
+            //if(ClawButton == -1){
+            //    Claw.setPosition(90);
+            //}
 
 
+            if (gamepad2.right_bumper) {
+                LongArm.setPower(1);
+            } else if (gamepad2.left_bumper) {
+                LongArm.setPower(-1);
 
-        if (gamepad2.right_trigger>0) {
-            intake.setPower(1);
-        }
+            }
 
-        if (gamepad2.left_trigger>0) {
-            intake.setPower(-1);
-            conveyor.setPower(-1);
-        }
 
-        if (beamBreak.getState()){
-            if  (gamepad2.left_trigger >0){
+            if (gamepad2.b) {
+                Shooter.setPower(1);            // This Controls the shooter
+                Stopper.setPosition(0.5);        // This sets the Stopper to allow rings to come in the Shooter
+            }
+
+
+            if (gamepad2.right_trigger > 0) {
+                intake.setPower(1);
+            }
+
+            if (gamepad2.left_trigger > 0) {
+                intake.setPower(-1);
                 conveyor.setPower(-1);
             }
-           else {
-                conveyor.setPower(1);
+
+            if (beamBreak.getState()) {
+                if (gamepad2.left_trigger > 0) {
+                    conveyor.setPower(-1);
+                } else {
+                    conveyor.setPower(1);
+                }
+            } else {
+                conveyor.setPower(0);        // This tells the program to set the Intake, Long Arm, and Shooter
+                intake.setPower(0);        //to turn them off when not being used
+                Stopper.setPosition(0);
+                LongArm.setPower(0);
+                Shooter.setPower(0);
+                LongArm.setPower(0);
             }
+
+
         }
-
-
-
-        else {
-            conveyor.setPower(0);        // This tells the program to set the Intake, Long Arm, and Shooter
-            intake.setPower(0);        //to turn them off when not being used
-            Stopper.setPosition(0);
-            LongArm.setPower(0);
-            Shooter.setPower(0);
-            LongArm.setPower(0);
-        }
-
-
-            }
-        }
+    }
+}
 
 
