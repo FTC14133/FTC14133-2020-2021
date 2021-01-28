eb7491997204f205e96a9440f0ed466db5aac8e7
Vihaan Chhabria <chhabria.vihaan@gmail.com>
2021-01-27 20:18:56 -0500

    Commit 28 Vihaan Chhabria - Updating Small Codes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Long_Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Long_Arm.java
@@ -1,55 +1,55 @@
-/**package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
 public class Long_Arm extends OpMode {
     private DcMotor LongArm = null;
+    static final double MOTOR_TICK_COUNT = 2800;
+    DigitalChannel LimitSwitchLongArm;          // Sets the variable of the LimitSwitchLongArm
 
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
-/**    @Override
+    @Override
     public void init() {
         LongArm = hardwareMap.get(DcMotor.class, "Long_Arm");
+        LongArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);        //Since this is the first time using the encoder we start it up
+
+        LongArm.setDirection(DcMotor.Direction.FORWARD);
+        LimitSwitchLongArm.setMode(DigitalChannel.Mode.INPUT);
+
     }
 
     @Override
     public void init_loop() {
     }
 
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
- /**   @Override
+    @Override
     public void start() {
 
-        /*
-         * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-         */
- //   }
+    }
 
- /**   @Override
+    @Override
     public void loop() {
-        if (gamepad1.right_bumper) {
-            LongArm.setDirection(DcMotor.Direction.FORWARD);        //sets the long arm forward
-            LongArm.setPower(3);
-        } else if (gamepad1.left_bumper) {
-            LongArm.setDirection(DcMotor.Direction.REVERSE);        //sets the long arm backwards
-            LongArm.setPower(3);
+
+        double armrotation = MOTOR_TICK_COUNT * (90 / 360);
+
+        if (gamepad2.right_bumper) {            //turns the arm that is long but there is not a arm that is short
+            LongArm.setTargetPosition((int) armrotation);        //Tell the motor to go to 90 degrees when told to
+            LongArm.setPower(1);        //Sets the power for the Long arm
+            LongArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         }
-        else {
-            LongArm.setPower(0);// This tells the program to set the Intake, Long Arm, and Shooter
-                                //to turn them off when not being used
+        if (gamepad2.left_bumper) {      //rotates the arm that is long but there is not a arm that is short
+            LongArm.setTargetPosition(0);        //Tell the motor to go to 90 degrees when told to
+            LongArm.setPower(1);        //Sets the power for the Long arm
+            LongArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         }
 
     }
 
 
 }
-  */
